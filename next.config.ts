b136f1babe022d3a7065fa99bd3704e2cc5d50c0
witiftydev <wit.ifty.dev@gmail.com>
2026-04-27 12:59:29 +0600

    ✅initial 4

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
